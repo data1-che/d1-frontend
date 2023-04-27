3f763e9d71ed75e057b0cfa016a645229bc07d14
data1-che <data1.hernan@gmail.com>
2023-04-27 18:30:51 -0300

    R1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from "@angular/fire/compat";
+/* import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireStorageModule } from "@angular/fire/storage"; */
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //AngularFireModule.initializeApp(environment.firebaseConfig),
+    /* AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule */
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'd1-frontend',
+    appId: '1:905114017898:web:fd4071aef1c52f039d2a13',
+    storageBucket: 'd1-frontend.appspot.com',
+    apiKey: 'AIzaSyBulj7Y90ntOn9UA8y7IYyJZ0tjFPcDks8',
+    authDomain: 'd1-frontend.firebaseapp.com',
+    messagingSenderId: '905114017898',
+    measurementId: 'G-RT071R3GD0',
+  },};
